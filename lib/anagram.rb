861eaeaaee7c37e4994ef3c3237b6ef98d6dab13
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-11-26 23:37:11 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,7 +19,7 @@ class Anagram
     array.map do |anagrams|
     # splitAnagram = anagrams.split("").sort
 # binding.pry
-    if splitWord === splitAnagram
+    if splitWord ===  anagrams.split("").sort
       anagrams
     end
       
